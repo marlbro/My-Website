605c2fea63ce34b600f6994a945154caa1b5118b
vvilli <hharriswilliam@gmail.com>
2020-12-29 16:31:39 -0600

    Added bio-photo(s), animations/page colors
    Very tricky CSS stuff going on; attempting to create a overlay on top of the bio-photos, using a lot of googled information and notes from the Udemy course.

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const photoButtons = document.querySelectorAll(".photo-buttons");
+const overlay = document.querySelector(".overlay");
+const photoRow = document.querySelector(".photo-row");
+
+// btnBioPhotos.addEventListener('click', function () {
+
+// };
