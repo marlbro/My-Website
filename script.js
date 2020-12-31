cff6d9d0f2a4ab354f9227c93ab80f7ab6a03465
vvilli <hharriswilliam@gmail.com>
2020-12-30 22:22:37 -0600

    added photo loop and hover animations
    trying to figure out how to align text under each highlighted photo, during hover. also trying to not have same photo pop up twice

--- script.js
+++ script.js
@@ -1,9 +1,61 @@
 "use strict";
 
-const photoButtons = document.querySelectorAll(".photo-buttons");
-const overlay = document.querySelector(".overlay");
-const photoRow = document.querySelector(".photo-row");
+/////////// things i want online
+/// 1) cycle through images for each button
+///
+/// 2) each button should have box with message display
+///
+/// 3) videos up on my site? music? randomly played things?
+///
+/// 4) professional work? this website is just a test of what ive learned
+///////////////////////
 
-// btnBioPhotos.addEventListener('click', function () {
+// const images = document.querySelectorAll(".image");
+// const overlay = document.querySelector(".overlay");
+// const photoRow = document.querySelector(".photo-container");
 
-// };
+// photo array
+const images = [
+  "./photo/photo-1.JPG",
+  "./photo/photo-1.JPG",
+  "./photo/photo-2.JPG",
+  "./photo/photo-3.JPG",
+];
+
+// photo element selection
+const centerBtn = document.getElementById("center");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
+const photo = document.querySelector(".image");
+
+// button element selection
+const musicBtn = document.querySelector(".music-button");
+const danceBtn = document.querySelector(".dance-button");
+const progBtn = document.querySelector(".prog-button");
+
+// 3 randomly generated numbers between array elements 1-3
+const randomNum = Math.trunc(Math.random() * 3) + 1;
+const randomNum1 = Math.trunc(Math.random() * 3) + 1;
+const randomNum2 = Math.trunc(Math.random() * 3) + 1;
+
+console.log(randomNum);
+console.log(leftBtn);
+
+// each element displays random photo
+leftBtn.src = `${images[randomNum]}`;
+rightBtn.src = `${images[randomNum1]}`;
+centerBtn.src = `${images[randomNum2]}`;
+
+// cancel out if one uses same photo
+if (leftBtn === rightBtn && centerBtn) {
+  leftBtn++;
+} else {
+}
+
+// loop through image array
+for (let i = 0; i < images.length; i++) {}
+
+// cycle through images for each button
+
+// const theLoop = function () {
+// for (i = 0; i < photoButtons[i]; i++);
